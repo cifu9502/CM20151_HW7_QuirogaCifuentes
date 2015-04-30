feaffb1f4b88c31a40e799f0de1dc2ac506f77a0
Jesús Cifuentes <jd.cifuentes998@gmail.com>
2015-04-30 15:23:44 -0500

    Delete dates.R

--- dates.R
+++
@@ -1,7 +0,0 @@
-library('lubridate')
-destfile <- "times.csv"
-dates <- read.csv(destfile, stringsAsFactors = FALSE)
-date <- as.POSIXct(dates[,2], tz = "GMT")
-options(digits = 12)
-date <- decimal_date(date) 
-write.csv(date, file = "tiempos.csv")
