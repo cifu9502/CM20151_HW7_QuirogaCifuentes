9ee2e6337abb3e01e9d4b18de42be4bd1bf9cd8b
Jesús Cifuentes <jd.cifuentes998@gmail.com>
2015-04-30 15:23:28 -0500

    Delete convert.sh

--- convert.sh
+++
@@ -1,6 +0,0 @@
-cat times-csv
-find -/type fexec sedi 's/-//g' {} \;
-find -/type fexec sedi 's/\//g' {} \;
-find -/type fexec sedi 's/\-/g' {} \;
-find -/type fexec sedi 's/\//g' {} \;
-find -/type fexec sedi 's/\ / /g' {} \;
