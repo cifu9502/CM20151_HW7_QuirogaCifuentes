2f16275081c728801529f2423813e6d57b4efb0f
cifu9502 <jd.cifuentes998@gmail.com>
2015-05-03 01:23:50 -0500

    Punto2 casi completo

--- Punto2/dates.R
+++ Punto2/dates.R
@@ -1,7 +1,8 @@
 library('lubridate')
-destfile <- "times3.csv"
+destfile <- "times2.csv"
 dates <- read.csv(destfile, stringsAsFactors = FALSE)
-date <- as.POSIXct(dates[,2], tz = "GMT")
+date <- as.POSIXct(dates[,2], , tz = "GMT")
 options(digits = 12)
-date <- decimal_date(date) 
+date <- decimal_date(date)
+
 write.csv(date, file = "tiempos.csv")
